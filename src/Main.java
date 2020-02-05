79c1cc879ad29b1c4962c3d6c648f7ccd3cf19cd
kanimisokanikani <kanimisokanikani0214@gmail.com>
2020-02-05 19:11:06 +0900

    獲得金額と投資額を表示するように 秒給 分給 時給を追加 当選まで掛かる秒数 分数 時間を追加 全体的に早くなったような気がしたりしなかったり

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.util.concurrent.TimeUnit;
-
 public class Main {
-    public static void main (String [] args) throws InterruptedException {
+    public static void main (String [] args) {
 
         /////////////
         // 変数作成 //
@@ -17,8 +15,13 @@ public class Main {
         //計算専用
         long stock_Calc;                //add_Stockを足したストック
         long bet_Calc;                  //一回の金額
-        double time_Calc;                 //一回転の時間
-        long number_Of_Times_calc = 0;  //回数
+        double seconds_Time_Calc;       //当選までの時間 (秒)
+        double minutes_Time_Calc;       //当選までの時間 (分)
+        double hours_Time_Calc;         //当選までの時間 (時間)
+        double secondly_Pay_Calc;       //秒給
+        double minutely_Pay_Calc;       //分給
+        double hourly_Pay_Calc;         //時給
+        long number_Of_Times_Calc = 0;  //回数
 
 
         /////////////
@@ -46,19 +49,30 @@ public class Main {
         /////////
 
         for (int i = 0; i < number_Of_Times; i++){
-            ++number_Of_Times_calc; //回転数を増やす (1回 2回 3回...)
+            ++number_Of_Times_Calc; //回転数を増やす (1回 2回 3回...)
 
-            stock_Calc = default_Stock + add_Stock * number_Of_Times_calc; //受け取るお金を制作 初期ストック + 追加ストック × 回数
-            bet_Calc = bet * number_Of_Times_calc; //使ったお金を制作 一回転の金額 × 回数
-            time_Calc = time * number_Of_Times_calc ; //かかった時間を制作 一回転の時間 × 回数
+            stock_Calc = default_Stock + add_Stock * number_Of_Times_Calc; //受け取るお金を制作 初期ストック + 追加ストック × 回数
+            bet_Calc = bet * number_Of_Times_Calc; //使ったお金を制作 一回転の金額 × 回数
+            seconds_Time_Calc = time * number_Of_Times_Calc ; //かかった時間(秒)を制作 一回転の時間 × 回数
+            minutes_Time_Calc = seconds_Time_Calc / 60 ; //かかった時間(分)を制作 かかった秒数 ÷ 60
+            hours_Time_Calc = minutes_Time_Calc / 60 ; //かかった時間(時間)を制作 かかった秒数 ÷ 60
+
+            secondly_Pay_Calc = stock_Calc / seconds_Time_Calc; //秒給を計算
+            minutely_Pay_Calc = secondly_Pay_Calc * 60; //分給を計算
+            hourly_Pay_Calc = minutely_Pay_Calc * 60; //時給を計算
 
             reduction_Rate = (stock_Calc + 0.0) / (bet_Calc + 0.0); //還元率を計算 受け取るお金 ÷ 使ったお金
             reduction_Rate *= 100.0; //％表記にするため100倍に
 
-            System.out.print(number_Of_Times_calc + "回転で当選した場合 還元率は " + reduction_Rate + " ％で、");
-            System.out.println("掛かる時間は " + time_Calc + " 秒です");
-
-            TimeUnit.MILLISECONDS.sleep(1);
+            System.out.println(number_Of_Times_Calc + "回転で当選した場合 還元率は " + reduction_Rate + " ％です。 ");
+            System.out.println("獲得金額は" + stock_Calc + " 円で、投資額は " + bet_Calc + " 円です。 ");
+            System.out.println("秒給は " + secondly_Pay_Calc + " 秒です");
+            System.out.println("分給は " + minutely_Pay_Calc + " 秒です");
+            System.out.println("時給は " + hourly_Pay_Calc + " 秒です");
+            System.out.println("掛かる秒数は " + seconds_Time_Calc + " 秒です");
+            System.out.println("掛かる分数は " + minutes_Time_Calc + " 分で、");
+            System.out.println("掛かる時間は " + hours_Time_Calc + " 時間です");
+            System.out.println("");
         }
     }
 }
